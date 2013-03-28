0c6ee9d3bdce35066121508d1bd6565aa719a9fc
Joe Cross <joe.mcross@gmail.com>
2013-03-27 18:52:19 -0700

    Port some of the PAdlib code
    Pulled in rrect and added alpha blending.

---
+++ padlib/__init__.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+def circle(surface, color, point, radius, width):
+    pygame.draw.circle(surface, color, point, radius, width)
+
+
+def rrect(surface, color, rect, radius, width):
+    '''
+    color is either (R,G,B) or (R,G,B,A) with each value between [0, 255]
+    rect is of the form (x, y, width, height) where x, y is the position of the upper left corner
+    radius is the curvature of the corners
+    '''
+    if color[0] + color[1] + color[2] == 0:
+        colorkey = (1, 1, 1)
+    else:
+        colorkey = (0, 0, 0)
+
+    surf_temp = pygame.Surface((rect[2], rect[3]), pygame.SRCALPHA)
+    surf_temp.fill(colorkey)
+    pygame.draw.rect(surf_temp, color, (0, radius, rect[2], rect[3]-2*radius), 0)
+    pygame.draw.rect(surf_temp, color, (radius, 0, rect[2]-2*radius, rect[3]), 0)
+
+    draw_circle = lambda point: circle(surf_temp, color, point, radius, 0)
+    points = [
+        [radius, radius],
+        [rect[2]-radius, radius],
+        [radius, rect[3]-radius],
+        [rect[2]-radius, rect[3]-radius]
+    ]
+    map(draw_circle, points)
+
+    if width != 0:
+        rrect(surf_temp, colorkey, (width, width, rect[2]-2*width, rect[3]-2*width), radius-width, 0)
+
+    surf_temp.set_colorkey(colorkey)
+    surface.blit(surf_temp, (rect[0], rect[1]))
