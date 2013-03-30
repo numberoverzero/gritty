74ed7fedb32e605a039bfc970e98ed6bb3192f33
Joe Cross <joe.mcross@gmail.com>
2013-03-29 20:22:36 -0700

    First CellCollection refactor
    Dropped Cell class, and updated CellCollection to use the new cell attr
    get/set methods.
    
    Still need to come up with a semi-performant / elegant way to handle
    attribute += and -=

--- lib/cell_collection.py
+++ lib/cell_collection.py
@@ -1,20 +1,6 @@
 import itertools
 
 
-class Cell(object):
-    def __init__(self, grid, pos):
-        self.grid = grid
-        self.pos = pos
-
-    @property
-    def color(self):
-        return self.grid.color_at(self.pos)
-
-    @color.setter
-    def color(self, w):
-        self.grid.color_at(self.pos, w)
-
-
 class CellCollection(object):
     def __init__(self, grid, cells=None):
             self.grid = grid
@@ -25,20 +11,12 @@ class CellCollection(object):
     def color(self):
         if len(self.cells) > 1:
             raise ValueError("Cannot return a single color for multiple cells")
-        return self.grid.get_color_at(self.cells[0])
+        return self.grid.get_attr_at('color', self.cells[0])
 
     @color.setter
     def color(self, c):
         for pos in self.cells:
-            self.grid.set_color_at(pos, c)
-
-    def on(self):
-        for pos in self.cells:
-            self.grid.on(pos)
-
-    def off(self):
-        for pos in self.cells:
-            self.grid.off(pos)
+            self.grid.set_attr_at('color', pos, c)
 
     def __iter__(self):
         for pair in itertools.product(range(self.rows), range(self.columns)):
