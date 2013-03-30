9cd254be9db5b3b65abb764e3a3cb8c856b852f2
Joe Cross <joe.mcross@gmail.com>
2013-03-30 01:41:00 -0700

    Major cell attribute rework
    Grid now supports global and local attributes (local to cells).  All
    iteration methods now return appropriate classes, since Cell is a thing.
    
    There's a lot of overhead in a few operations: specifically, painting a
    rectangle (all the back and forth get/set for computing the rectangle
    bounds, twice!)
    Also, the naming convention for adjusting global/local attributes is
    pretty crappy.  There's 6 methods:
    
    set_cell_attribute_default
    get_cell_attribute_default
    has_cell_attribute
    set_global_attribute
    get_global_attribute
    del_global_attribute
    
    This needs to be cut down, but I need a way to track changes and
    differentiate global from local settings.  Maybe the interface can be
    has_attr(self, scope, name)
    get_attr(self, scope, name)
    set_attr(self, scope, name, value)
    
    If deletes are only for global attributes, I'm not sure they need to be
    there.
    This does require you to pass 'global' or 'cell' in all over (probably
    changing 'cell' to 'local') but we'll see.

--- lib/cell_collection.py
+++ lib/cell_collection.py
@@ -1,31 +1,89 @@
-import itertools
+from gritty.padlib import rrect
+
+
+class Cell(object):
+    def __init__(self, grid, pos, attrs):
+        self.grid = grid
+        self.pos = pos
+        for name, value in attrs.iteritems():
+            setattr(self, name, value)
+
+    @property
+    def _rect(self):
+        '''Returns the (x, y, width, height) rectangle of the given cell'''
+        border_size = self.grid.get_global_attribute('cell_border_size')
+        width = self.grid.get_global_attribute('cell_width')
+        height = self.grid.get_global_attribute('cell_height')
+        x, y = self.pos
+        x = border_size * (1 + x) + width * x
+        y = border_size * (1 + y) + height * y
+        return [x, y, width, height]
+
+    def draw(self, surface):
+        border_size = self.grid.get_global_attribute('cell_border_size')
+        border_color = self.border_color
+
+        #Border
+        border_rect = self._rect
+        border_rect[0] -= border_size
+        border_rect[1] -= border_size
+        border_rect[2] += 2 * border_size
+        border_rect[3] += 2 * border_size
+        rrect(surface, border_color, border_rect, 0, 0)
+
+        #Cell
+        color = self.color
+        rect = self._rect
+        rrect(surface, color, rect, self.radius, 0)
+
+    def __setattr__(self, name, value):
+        if name == 'grid':
+            object.__setattr__(self, name, value)
+        elif self.grid.has_cell_attribute(name):
+            object.__setattr__(self, name, value)
+            self.grid.update_cell(self)
+        else:
+            object.__setattr__(self, name, value)
+
+    def __str__(self):
+        return "Cell({}, {})".format(self.pos, self.color)
+
+    def __repr__(self):
+        return str(self)
 
 
 class CellCollection(object):
     def __init__(self, grid, cells=None):
-            self.grid = grid
-            if cells:
-                self.cells = list(cells)
+        self._grid = grid
+        if cells:
+            self._cells = cells
+        else:
+            self._cells = []
 
-    @property
-    def color(self):
-        if len(self.cells) > 1:
-            raise ValueError("Cannot return a single color for multiple cells")
-        return self.grid.get_attr_at('color', self.cells[0])
+    def __getattr__(self, name):
+        if name in ['_grid', '_cells']:
+            return object.__getattr__(self, name)
+        get = lambda c: getattr(c, name)
+        return map(get, self._cells)
 
-    @color.setter
-    def color(self, c):
-        for pos in self.cells:
-            self.grid.set_attr_at('color', pos, c)
+    def __setattr__(self, name, value):
+        if name in ['_grid', '_cells']:
+            object.__setattr__(self, name, value)
+        else:
+            set = lambda c: setattr(c, name, value)
+            map(set, self._cells)
 
     def __iter__(self):
-        for pair in itertools.product(range(self.rows), range(self.columns)):
-            yield pair[::-1]
+        return self._cells
 
     def __add__(self, other):
-        cells = sorted(set(self.cells + other.cells))
-        return CellCollection(self.grid, cells)
+        #Use cell position instead of object in case one of the lists updated a cell value
+        positions = set([c.pos for c in self] + [c.pos for c in other])
+        cells = [self.grid.cell_at(pos) for pos in positions]
+        return CellCollection(self._grid, cells)
 
     def __sub__(self, other):
-        cells = sorted(set(self.cells) - set(other.cells))
-        return CellCollection(self.grid, cells)
+        #Use cell position instead of object in case one of the lists updated a cell value
+        positions = set(c.pos for c in self) - set(c.pos for c in other)
+        cells = [self.grid.cell_at(pos) for pos in set(positions)]
+        return CellCollection(self._grid, cells)
--- lib/grid.py
+++ lib/grid.py
@@ -1,7 +1,6 @@
 import itertools
 import pygame
-from gritty.padlib import rrect
-from cell_collection import CellCollection
+from cell_collection import Cell, CellCollection
 
 DEFAULT_CELL_COLOR = (0, 0, 0, 255)
 DEFAULT_BORDER_COLOR = (0, 0, 0, 255)
@@ -15,85 +14,68 @@ class Grid(object):
         rows:: the number of rows in the grid
         columns:: the number of columns in the grid
 
-        keyword arguments:
-        cell_color_default :: default color a cell is rendered with
-        border_color   :: border color for the grid (exterior edges and border between cells)
+        keyword arguments are split into Global attributes and Cell attributes.
 
-        border_size    :: thickness of the border
-        cell_radius    :: curvature of the cell corners.  Use 0 for square corners
+        Global attributes are features applicable to the grid as a whole, or apply to every cell.
+        Adding, removing, or updating global attributes will force a redraw of the grid.
+
+        Cell attributes are features whose value can change per-cell.
+        Adding, removing, or updating a cell attribute will force a redraw of that cell.
+        Changing a cell attribute default will force a redraw of the grid.
+
+        GLOBAL
+        cell_width         :: width in pixels of a cell
+        cell_height        :: height in pixels of a cell
+        cell_border_size   :: thickness in pixels of the border
+
+        CELL
+        cell_color_default :: default color a cell is rendered with
+        cell_radius        :: curvature of the cell corners.  Use 0 for square corners
+        border_color       :: border color for the grid (exterior edges and border between cells)
         '''
         self._rows = rows
         self._columns = columns
-        self._cells = {}
 
-        self._defaults = {
-            'color': kwargs.get('cell_color_default', DEFAULT_CELL_COLOR)
+        self._global_attributes = {
+            'cell_width': cell_width,
+            'cell_height': cell_height,
+            'cell_border_size': kwargs.get('cell_border_size', DEFAULT_BORDER_SIZE)
         }
 
-        self._border_color = kwargs.get('border_color', DEFAULT_BORDER_COLOR)
+        self._cell_attributes = {
+            'color': kwargs.get('cell_color_default', DEFAULT_CELL_COLOR),
+            'radius': kwargs.get('cell_radius', DEFAULT_CELL_RADIUS),
+            'border_color': kwargs.get('cell_border_color', DEFAULT_BORDER_COLOR)
+        }
 
-        self._border_size = kwargs.get('border_size', DEFAULT_BORDER_SIZE)
-
-        self._cell_radius = kwargs.get('cell_radius', DEFAULT_CELL_RADIUS)
-        self._cell_width = cell_width
-        self._cell_height = cell_height
-
-        self._surf_cache = pygame.Surface(self._render_dimensions, pygame.SRCALPHA)
-        self._init_surf_cache()
-        self._dirty = list(self)
-
-    def _cell(self, pos):
-        if pos not in self._cells:
-            return None
-        return self._cells[pos]
+        self._cells = {}
+        self._dirty = []
+        self._surf_cache = pygame.Surface(self.render_dimensions, pygame.SRCALPHA)
+        self._forced_redraw = False
+        self.force_redraw()
 
     @property
-    def _render_dimensions(self):
-        width = self._columns * self._cell_width + (self._columns + 1) * self._border_size
-        height = self._rows * self._cell_height + (self._rows + 1) * self._border_size
+    def render_dimensions(self):
+        cell_width = self.get_global_attribute('cell_width')
+        cell_height = self.get_global_attribute('cell_height')
+        border_size = self.get_global_attribute('cell_border_size')
+        width = self._columns * cell_width + (self._columns + 1) * border_size
+        height = self._rows * cell_height + (self._rows + 1) * border_size
         return width, height
-
-    def _cell_rect(self, (x, y)):
-        '''Returns the (x, y, width, height) rectangle of the given cell'''
-        x = self._border_size * (1 + x) + self._cell_width * x
-        y = self._border_size * (1 + y) + self._cell_height * y
-        return (x, y, self._cell_width, self._cell_height)
-
-    def _init_surf_cache(self):
-        for pos in self:
-            self._draw_cell(self._surf_cache, pos)
-
-    def _draw_cell(self, surface, pos):
-        x, y = pos
-        x *= (self._border_size + self._cell_width)
-        y *= (self._border_size + self._cell_height)
-
-        #Border
-        rect = (x, y,
-                self._cell_width + 2 * self._border_size,
-                self._cell_height + 2 * self._border_size
-                )
-        rrect(surface, self._border_color, rect, 0, 0)
-
-        #Cell
-        x += self._border_size
-        y += self._border_size
-        color = self.get_attr_at('color', pos)
-        rect = (x, y, self._cell_width, self._cell_height)
-        rrect(surface, color, rect, self._cell_radius, 0)
 
     @property
     def surface(self):
         if self._dirty:
-            for pos in self._dirty:
-                self._draw_cell(self._surf_cache, pos)
+            self._forced_redraw = False
+            for cell in self._dirty:
+                cell.draw(self._surf_cache)
             self._dirty = []
         return self._surf_cache
 
     def hit_check(self, pos):
         '''Returns the cell pos that contains pos, or None'''
         cell_width, cell_height = self._cell_width, self._cell_height
-        width, height = self._render_dimensions
+        width, height = self.render_dimensions
         border_size = self._border_size
         pos_x, pos_y = pos
 
@@ -109,30 +91,42 @@ class Grid(object):
             return None
         return x, y
 
-    @property
-    def all(self):
-        return CellCollection(self, self)
+    def update_cell(self, cell):
+        self._cells[cell.pos] = cell
+        self._dirty.append(cell)
 
-    def get_attr_at(self, attr, pos):
-        '''Gets the color at a position'''
-        cell = self._cell(pos)
-        if not cell or attr not in cell:
-            return self._defaults.get(attr, None)
-        return list(cell[attr])
+    def force_redraw(self):
+        if not self._forced_redraw:
+            for cell in self:
+                self.update_cell(cell)
 
-    def set_attr_at(self, attr, pos, value):
-        '''Sets the color at a position'''
-        cell = self._cell(pos)
-        if cell is None:
-            self._cells[pos] = cell = {}
-        old_value = self.get_attr_at(attr, pos)
-        cell[attr] = list(value)
-        if old_value != value:
-            self._dirty.append(pos)
+    def cell_at(self, pos):
+        return self._cells.get(pos, Cell(self, pos, self._cell_attributes))
+
+    def set_cell_attribute_default(self, name, default):
+        self._cell_attributes[name] = default
+        self.force_redraw()
+
+    def get_cell_attribute_default(self, name):
+        return self._cell_attributes[name]
+
+    def has_cell_attribute(self, name):
+        return name in self._cell_attributes
+
+    def set_global_attribute(self, name, value):
+        self._global_attributes[name] = value
+        self.force_redraw()
+
+    def get_global_attribute(self, name):
+        return self._global_attributes[name]
+
+    def del_global_attribute(self, name):
+        del self._global_attributes[name]
+        self.force_redraw()
 
     def __iter__(self):
-        for pair in itertools.product(range(self._rows), range(self._columns)):
-            yield pair[::-1]
+        for revpos in itertools.product(range(self._rows), range(self._columns)):
+            yield self.cell_at(revpos[::-1])
 
     def __getitem__(self, (x, y)):
         if isinstance(x, slice):
@@ -143,6 +137,6 @@ class Grid(object):
             ys = range(*y.indices(self._rows))
         else:
             ys = [y]
-        pairs = itertools.product(ys, xs)
-        pairs = (tuple(reversed(pair)) for pair in pairs)
-        return CellCollection(self, pairs)
+        pairs = (pos[::-1] for pos in itertools.product(ys, xs))
+        cells = map(self.cell_at, pairs)
+        return CellCollection(self, cells)
