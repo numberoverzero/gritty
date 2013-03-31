5735fd048391b9785d5713e190dea1cab137f633
Joe Cross <joe.mcross@gmail.com>
2013-03-30 17:55:54 -0700

    Corrected Cell, CellCollection iter
    Added add, sub to Cell for Cell/Cell addition + subtraction.

--- lib/cell_collection.py
+++ lib/cell_collection.py
@@ -2,30 +2,30 @@
 
 class Cell(object):
     def __init__(self, grid, pos):
-        self.grid = grid
+        self._grid = grid
         self.pos = pos
 
     def __setattr__(self, name, value):
-        if name == 'grid':
+        if name == '_grid':
             object.__setattr__(self, name, value)
-        elif name in self.grid.cell_attr:
-            if name in self.grid.cell_attr_coercion_funcs:
-                value = self.grid.cell_attr_coercion_funcs[name](value)
+        elif name in self._grid.cell_attr:
+            if name in self._grid.cell_attr_coercion_funcs:
+                value = self._grid.cell_attr_coercion_funcs[name](value)
             object.__setattr__(self, name, value)
-            self.grid.update_cell(self)
+            self._grid.update_cell(self)
         else:
             object.__setattr__(self, name, value)
 
     def __getattr__(self, name):
-        if name == 'grid':
+        if name == '_grid':
             return object.__getattr__(self, name)
-        elif name in self.grid.cell_attr:
+        elif name in self._grid.cell_attr:
             try:
                 value = object.__getattr__(self, name)
             except AttributeError:
-                value = self.grid.cell_attr[name]
-            if name in self.grid.cell_attr_coercion_funcs:
-                value = self.grid.cell_attr_coercion_funcs[name](value)
+                value = self._grid.cell_attr[name]
+            if name in self._grid.cell_attr_coercion_funcs:
+                value = self._grid.cell_attr_coercion_funcs[name](value)
             return value
         else:
             return object.__getattr__(self, name)
@@ -38,7 +38,13 @@ class Cell(object):
         return str(self)
 
     def __iter__(self):
-        yield self
+        return iter([self])
+
+    def __sub__(self, other):
+        return CellCollection(self._grid, self) - other
+
+    def __add__(self, other):
+        return CellCollection(self._grid, self) - other
 
 
 class CellCollection(object):
@@ -63,16 +69,22 @@ class CellCollection(object):
             map(set, self._cells)
 
     def __iter__(self):
-        return self._cells
+        return iter(self._cells)
 
     def __add__(self, other):
         #Use cell position instead of object in case one of the lists updated a cell value
         positions = set([c.pos for c in self] + [c.pos for c in other])
-        cells = [self.grid.cell_at(pos) for pos in positions]
+        cells = [self._grid.cell_at(pos) for pos in positions]
         return CellCollection(self._grid, cells)
 
     def __sub__(self, other):
         #Use cell position instead of object in case one of the lists updated a cell value
         positions = set(c.pos for c in self) - set(c.pos for c in other)
-        cells = [self.grid.cell_at(pos) for pos in set(positions)]
+        cells = [self._grid.cell_at(pos) for pos in set(positions)]
         return CellCollection(self._grid, cells)
+
+    def __str__(self):
+        return str(list(self))
+
+    def __repr__(self):
+        return str(self)
