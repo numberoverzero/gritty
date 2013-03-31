85b4f1318d18d74f9a41fb4d71ae000698841faf
Joe Cross <joe.mcross@gmail.com>
2013-03-31 13:29:00 -0700

    Factor out basic demo grid setup
    This code is in every demo in some way - pulling it out standardizes the
    grid used and makes it easier to setup a demo and for others to read
    through the code (I think)

---
+++ gritty/demos/__init__.py
@@ -0,0 +1,39 @@
+import pygame
+from gritty import Grid
+
+# gritty demo
+# Copyright 2013 Joe Cross
+# This is free software, released under The GNU Lesser General Public License, version 3.
+# You are free to use, distribute, and modify pyGrid. If modification is your game,
+# it is recommended that you read the GNU LGPL license: http://www.gnu.org/licenses/
+
+
+def basic_grid():
+    rows = 40
+    columns = 40
+    cell_width = 20
+    cell_height = 20
+    COLOR_OFF = (000, 000, 255)
+    COLOR_ON = (255, 255, 51)
+
+    args = [
+        rows,
+        columns,
+        cell_width,
+        cell_height
+    ]
+
+    kwargs = {
+        'cell_color_default': COLOR_OFF,
+        'cell_border_color': (000, 000, 000),
+        'cell_border_size': 3,
+        'cell_radius': 0,
+    }
+
+    grid = Grid(*args, **kwargs)
+    pygame.init()
+    pygame.display.set_caption("Click to toggle, space to pause/resume")
+    screen = pygame.display.set_mode(grid.render_dimensions)
+    background_color = (255, 255, 255)
+    screen.fill(background_color)
+    return grid, screen, COLOR_OFF, COLOR_ON
