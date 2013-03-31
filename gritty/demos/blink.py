c5f44d8223209e29eaa0a194d997d6fb0a3668a3
Joe Cross <joe.mcross@gmail.com>
2013-03-31 15:39:53 -0700

    Blink demo uses basic_grid

--- gritty/demos/blink.py
+++ gritty/demos/blink.py
@@ -1,5 +1,5 @@
 import pygame
-from gritty import Grid
+from gritty.demos import basic_grid
 
 # gritty demo
 # Copyright 2013 Joe Cross
@@ -7,36 +7,13 @@ from gritty import Grid
 # You are free to use, distribute, and modify pyGrid. If modification is your game,
 # it is recommended that you read the GNU LGPL license: http://www.gnu.org/licenses/
 
-rows = 9
-columns = 9
-cell_width = 50
-cell_height = 50
-COLOR_OFF = [000, 000, 255, 255]
-COLOR_ON = [255, 255, 51, 255]
+grid, screen, COLOR_OFF, COLOR_ON = basic_grid()
 
-args = [
-    rows,
-    columns,
-    cell_width,
-    cell_height
-]
-
-kwargs = {
-    'cell_color_default': COLOR_OFF,
-    'cell_border_color': (000, 000, 000, 255),
-    'cell_border_size': 5,
-    'cell_radius': 0,
-}
-
-grid = Grid(*args, **kwargs)
-grid_pos = (0, 0)
-pygame.init()
-pygame.display.set_caption("Blink example")
-screen = pygame.display.set_mode(grid.render_dimensions)
 background_color = (255, 255, 255)
+blink_selection = grid[2:grid.rows:6, 2:grid.columns:6]
 
-blink_selection = grid[1:10:3, 1:10:3]
-blink_selection.color = COLOR_ON
+# Add some yellow lines for the hell of it
+(grid[2::12, :] + grid[:, 2::12]).color = COLOR_ON
 
 
 def clear():
@@ -44,7 +21,7 @@ def clear():
 
 
 def draw_grid():
-    screen.blit(grid.surface, grid_pos)
+    screen.blit(grid.surface, (0, 0))
 
 alpha = 255
 factor = 0.99
@@ -58,15 +35,14 @@ while True:
 
     alpha *= factor
     if alpha > 200:
-        factor = 0.9
+        factor = 0.95
         alpha = 200
     if alpha < 20:
-        factor = 1.1
+        factor = 1.05
         alpha = 20
 
-    color = list(COLOR_ON)
-    color[-1] = int(alpha)
-    blink_selection.color = color
+    COLOR_OFF[-1] = int(alpha)
+    blink_selection.color = COLOR_OFF
 
     clear()
     draw_grid()
