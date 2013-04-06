5fcd19c9927db452015b52738fce16996bd759eb
Joe Cross <joe.mcross@gmail.com>
2013-04-05 20:31:07 -0700

    Added Color class
    Color supports swizzling and indexing, with a default ordering of rgba.
    
    swizzling means you can access the colors in any order, in case you want
    blue then alpha then green and no red, you can use my_color.bag and get
    a list of length three back.
    
    grid is going to get a bit of an overhaul in the way of a pluggable
    renderer.  This will be a pretty big overhaul, but afterwards it will be
    easy for people to plug their own rendering in instead of being forced
    to use color.  color will either be an available renderer, or the
    default, so that it's easy for people to get started with grid.

--- gritty/lib.py
+++ gritty/lib.py
@@ -4,6 +4,7 @@
 # You are free to use, distribute, and modify gritty. If modification is your
 # game, it is recommended that you read the GNU LGPL license:
 # http://www.gnu.org/licenses/
+import re
 
 
 def coerce_alpha(input, *args, **kwargs):
@@ -16,8 +17,7 @@ def coerce_alpha(input, *args, **kwargs):
 
 class NotifiableDict(dict):
     def __init__(self, iterable=None, **kwargs):
-        if iterable is None:
-            iterable = []
+        iterable = iterable or []
         dict.__init__(self, iterable, **kwargs)
         self._notify_func = lambda *a, **kw: None
 
@@ -34,3 +34,50 @@ class NotifiableDict(dict):
         old_value = self.get(key, None)
         dict.__delitem__(self, key)
         self._notify_func(key, old_value, None)
+
+_rgba = 'rgba'
+_channel_search = re.compile('[^rgba]').search
+_multi_channel = lambda name: len(name) > 1 and not bool(_channel_search(name))
+_oga, _osa = object.__getattr__, object.__setattr__
+
+
+class Color(object):
+    __slots__ = ['r', 'g', 'b', 'a']
+
+    def __init__(self, r=0, g=0, b=0, a=255):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+
+    def __iter__(self):
+        return iter(self.rgba)
+
+    def __getitem__(self, index):
+        return list(self)[index]
+
+    def __setitem__(self, index, value):
+        if isinstance(index, slice):
+            indices = range(*index.indices(4))
+        else:
+            indices = [index]
+        func = lambda i: setattr(self, _rgba[i], value)
+        map(func, indices)
+
+    def __getattr__(self, name):
+        if _multi_channel(name):
+            return list(_oga(self, ch) for ch in name)
+        return _oga(self, name)
+
+    def __setattr__(self, name, value):
+        if _multi_channel(name):
+            func = lambda ch, v: _osa(self, ch, v)
+            map(func, zip(name, value))
+        else:
+            _osa(self, name, value)
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return "Color({}, {}, {}, {})".format(*self)
